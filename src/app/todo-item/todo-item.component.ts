cafda642120c215896e96cc9ccb9cea4a42c8e7b
lilui <luisa.schleu@outlook.com>
2020-05-23 17:48:34 +0200

    chapter 11 interfaces

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {TodoItem} from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,7 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  @Input() item;
+  @Input() item: TodoItem;
 
   constructor() { }
 
